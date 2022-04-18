0f463ddb23370e64b68b31f61bfbc4265b53dfa3
thomas zundel <thomas.zundel@student.centralia.edu>
2022-04-17 19:27:49 -0700

    Add project files.

---
+++ AEMS_Zundel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AEMS_Zundel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
